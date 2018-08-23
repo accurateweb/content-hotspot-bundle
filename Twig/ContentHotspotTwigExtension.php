0959c839b118dfd4571738097e240fa1d81f3c6c
eobuhov <eobuhov@accurateweb.ru>
2018-08-23 20:38:50 +0500

    + canEdit

--- Twig/ContentHotspotTwigExtension.php
+++ Twig/ContentHotspotTwigExtension.php
@@ -4,18 +4,22 @@ namespace Accurateweb\ContentHotspotBundle\Twig;
 
 use Accurateweb\ContentHotspotBundle\Model\ContentHotspot;
 use Accurateweb\ContentHotspotBundle\Model\ContentHotspotInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Doctrine\ORM\EntityManagerInterface;
 use Sonata\AdminBundle\Admin\AdminInterface;
 
 class ContentHotspotTwigExtension extends \Twig_Extension
 {
-  private $em, $clickZoneAdmin, $clickZoneEntity;
- 
-  public function __construct(EntityManagerInterface $manager, AdminInterface $admin, $entity)
+  private $em, $clickZoneAdmin, $clickZoneEntity, $session, $provider, $canEdit;
+  
+  public function __construct(EntityManagerInterface $manager, AdminInterface $admin, $entity, Session $session, $provider)
   {
     $this->em = $manager;
     $this->clickZoneAdmin = $admin;
     $this->clickZoneEntity = $entity;
+    $this->provider = $provider;
+    $this->session = $session;
+    $this->setCanEdit();
   }
   
   public function getFunctions()
@@ -28,9 +32,29 @@ class ContentHotspotTwigExtension extends \Twig_Extension
   
   public function getHotspot(\Twig_Environment $environment, $alias, $defaultContent = '')
   {
-    $clickZone =  $this->em->getRepository($this->clickZoneEntity)->findOneBy(['alias' => $alias]);
+    $canEdit = false;
     
-    if(!$clickZone)
+    $provider = '_security_' . $this->provider;
+    
+    $usernamePasswordToken = $this->session->get($provider);
+    $usernamePasswordToken = unserialize($usernamePasswordToken);
+    
+    if ($usernamePasswordToken !== false)
+    {
+      /** @var Symfony\Component\Security\Core\Role\Role $role */
+      foreach ($usernamePasswordToken->getRoles() as $role)
+      {
+        if ($role->getRole() === "ROLE_ADMIN")
+        {
+          $canEdit = true;
+          break;
+        }
+      }
+    }
+    
+    $clickZone = $this->em->getRepository($this->clickZoneEntity)->findOneBy(['alias' => $alias]);
+    
+    if (!$clickZone)
     {
       /** @var ContentHotspotInterface $clickZone */
       $clickZone = new $this->clickZoneEntity();
@@ -46,14 +70,15 @@ class ContentHotspotTwigExtension extends \Twig_Extension
     return $environment->render('@ContentHotspot/content_hotspot_default.html.twig', array(
       'adminPath' => $this->clickZoneAdmin->generateUrl('edit', ['id' => $alias]),
       'clickZone' => $clickZone,
+      'canEdit' => $canEdit
     ));
   }
   
   public function getInlineHotspot(\Twig_Environment $environment, $alias, $defaultContent = '')
   {
-    $clickZone =  $this->em->getRepository($this->clickZoneEntity)->findOneBy(['alias' => $alias]);
+    $clickZone = $this->em->getRepository($this->clickZoneEntity)->findOneBy(['alias' => $alias]);
     
-    if(!$clickZone)
+    if (!$clickZone)
     {
       /** @var ContentHotspotInterface $clickZone */
       $clickZone = new $this->clickZoneEntity();
@@ -69,6 +94,30 @@ class ContentHotspotTwigExtension extends \Twig_Extension
     return $environment->render('@ContentHotspot/content_hotspot_inline.html.twig', array(
       'adminPath' => $this->clickZoneAdmin->generateUrl('edit', ['id' => $alias]),
       'clickZone' => $clickZone,
+      'canEdit' => $this->canEdit
     ));
+  }
+  
+  public function setCanEdit()
+  {
+    $this->canEdit = false;
+    
+    $provider = '_security_' . $this->provider;
+    
+    $usernamePasswordToken = $this->session->get($provider);
+    $usernamePasswordToken = unserialize($usernamePasswordToken);
+    
+    if ($usernamePasswordToken !== false)
+    {
+      /** @var Symfony\Component\Security\Core\Role\Role $role */
+      foreach ($usernamePasswordToken->getRoles() as $role)
+      {
+        if ($role->getRole() === "ROLE_ADMIN")
+        {
+          $this->canEdit = true;
+          break;
+        }
+      }
+    }
   }
 }
