271a0f1027e19005c2b2516bfebb7684d5b8eb28
viktor <vkuklev@accurateweb.ru>
2019-06-25 13:56:22 +0500

    *Изменил toolbar клик-зоны с tinymce.
    (cherry picked from commit 92912db)

--- Resources/public/js/editor/clickzone-editor.js
+++ Resources/public/js/editor/clickzone-editor.js
@@ -4,17 +4,17 @@
  * @revision SVN: $Revision$ 
  */
 (function($, undefined){
-  
-  
+
+
   // получим названием контроллера из пути
   var urlParts = window.location.pathname.split('/');
-  
+
   var prefix = '';
-  
+
   if(urlParts[1] !== undefined && urlParts[1].match(/^\w+\.php$/i)) {
     prefix = '/' + urlParts[1];
   }
-  
+
   var save = function(alias, text, success, error){
     $.ajax({
       url: prefix + '/clickzones/'+ alias,
@@ -31,38 +31,47 @@
       }
     });
   }
-  
+
   tinymce.init({
     selector: ".clickzone:not(.clickzone-inline)",
+    language_url : '/js/vendor/tinymce/langs/ru.js',
+    branding: false,
+    plugins: [
+      "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
+      "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+      "save table contextmenu directionality emoticons template paste textcolor"
+    ],
+    toolbar1: 'undo redo | styleselect | bold italic strikethrough forecolor backcolor | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
+    toolbar2: 'link image video',
+    image_advtab: true ,
+    relative_urls: false,
     inline: true,
     language: 'ru',
-//    toolbar: "undo redo",
-//    menubar: false,
     setup : function(ed) {
       var changed = false,
           alias = $(ed.getElement()).attr('id');
-          
+
       ed.on('change', function(e){
         changed = true;
       }).on('blur', function(e) {
-          if (changed){
-            changed = false;
-            
-            save(alias, e.target.getContent(), 
-              function(r){}, 
+        if (changed){
+          changed = false;
+
+          save(alias, e.target.getContent(),
+              function(r){},
               function(){
                 changed = true;
               }
-            );
-          }
+          );
+        }
       }).on('');
-   }
+    }
   });
   $(function(){
     $('.clickzone-inline').each(function(){
-      var editing = false,        
+      var editing = false,
           self = $(this);
-      self.click(function(e){      
+      self.click(function(e){
         if (!editing){
           editing = true;
 
@@ -73,7 +82,7 @@
             editing = false;
             save(self.attr('id'), text, function(){}, function(){});
             $input.off().remove();
-            
+
             if (text.length){
               self.removeClass('clickzone-empty');
               self.text(text);
